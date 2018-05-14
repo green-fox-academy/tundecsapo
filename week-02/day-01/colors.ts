46db56df26b81d71009700dd24477f05a6e4f023
Tunde Csapo <tunde.csapo828@gmail.com>
2018-05-14 15:22:00 +0200

    Solution for Colors added

---
+++ week-02/day-01/colors.ts
@@ -0,0 +1,12 @@
+'use strict';
+export{};
+
+let colors: string[][] = [
+    ["lime", "forest green", "olive", "pale green", "spring green"], 
+    ["orange red", "red", "tomato"], 
+    ["orchid", "violet", "pink", "hot pink"]
+];
+
+console.log(colors[0])
+console.log(colors[1])
+console.log(colors[2])
