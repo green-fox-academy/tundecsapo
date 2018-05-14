df6050d6efd5915d7db9cc93ba102a3860c6feb6
Tunde Csapo <tunde.csapo828@gmail.com>
2018-05-14 12:37:20 +0200

    'Compare Length' solution added

---
+++ week-02/day-01/compare-length.ts
@@ -0,0 +1,9 @@
+let firstList: number[] = [1, 2, 3];
+let secondList: number[] = [4, 5];
+
+if (secondList.length > firstList.length){
+    console.log('secondList has more elements than firstList')
+}
+/*else {
+    console.log('firstList has more elements than secondList')
+}*/
