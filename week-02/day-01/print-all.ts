8d94476e955e6333d8e317574f3a1fcf7c87f277
Tunde Csapo <tunde.csapo828@gmail.com>
2018-05-14 13:45:43 +0200

    Solution for 'Print Elements' added

---
+++ week-02/day-01/print-all.ts
@@ -0,0 +1,14 @@
+'use strict';
+export{};
+
+let integers: number[] = [4, 5, 6, 7];
+
+for (let i: number = 0; i < integers.length; i++){
+    console.log(integers[i]);
+}
+
+/* Other solution:
+integers.forEach(function(element){
+    console.log(element)
+});
+*/
