56100681e1f8ea6aa674153ead6ad0071218b5f3
Tunde Csapo <tunde.csapo828@gmail.com>
2018-05-15 13:36:47 +0200

    Solution for '05 Todo print' added

---
+++ week-02/day-02/todo-print.ts
@@ -0,0 +1,8 @@
+'use strict';
+export{};
+
+let todoText: string = ' - Buy milk\n';
+todoText = `My todo:\n${todoText} - Download games\n     - Diablo`;
+
+console.log(todoText);
+//or we can save the different parts into variables and concat into a string but it would be longer
