0436cee1d0a78f535ddba1927929a13fd87bf4bf
Tunde Csapo <tunde.csapo828@gmail.com>
2018-05-16 19:22:09 +0200

    Solution for 'Doubled' added

---
+++ week-02/day-03/doubled.ts
@@ -0,0 +1,34 @@
+'use strict';
+export{};
+
+declare function require(path: string): any;
+const fs = require('fs');
+let charEncoding = 'utf-8';
+
+function readFromFile(filePath: string) {
+  try {
+    return fs.readFileSync(filePath, charEncoding);
+  } catch(error) {
+    console.log(`Unable to read file: ${filePath}`);
+    return null;
+  }
+}
+
+function writeToFile(file: string, newText: string) {
+  try {
+    console.log('File writing succesful!');
+    return fs.writeFileSync(file, newText);
+  } catch(error) {
+    return null;  
+  }
+}
+
+function checkDuplicates(doubledText: string) {
+  let content: string = readFromFile(doubledText);
+  let newStr: string = '';
+  for (let i: number = 0; i < content.length; i+=2) {
+    newStr = newStr.concat(content[i]);    
+  }
+  writeToFile(doubledText, newStr);
+}
+checkDuplicates('duplicated-chars.txt');
