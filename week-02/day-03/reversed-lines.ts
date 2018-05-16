b4b1f75ab9e2b3bc94ad981bcd0f7eadaf474018
Tunde Csapo <tunde.csapo828@gmail.com>
2018-05-16 20:45:57 +0200

    Solution for 'Reversed lines' added

--- week-02/day-03/reversed-lines.ts
+++ week-02/day-03/reversed-lines.ts
@@ -19,16 +19,24 @@ function writeToFile(file: string, newText: string) {
     console.log('File writing succesful!');
     return fs.writeFileSync(file, newText);
   } catch(error) {
+    console.log('Something went wrong while writing the file!');
     return null;  
   }
 }
 
 function reverseText(revLines: string) {
   let content: string = readFromFile(revLines);
+  let testArr: string[] = content.split('\r\n');
+  let finalArr: string[] = [];
   let newStr: string = '';
-  for (let i: number = content.length-1; i >=0; i--) {
-    newStr = newStr.concat(content[i]);    
+  for (let i: number = 0; i < testArr.length; i++) {
+    for (let j: number = testArr[i].length-1; j >=0; j--) {
+      newStr = newStr.concat(testArr[i][j]);    
+    }
+    finalArr.push(newStr);
+    newStr = '';
   }
-  writeToFile(revLines, newStr);
+  let finalText: string = finalArr.join('\r\n');
+  writeToFile(revLines, finalText);
 }
 reverseText('reversed-lines.txt');
