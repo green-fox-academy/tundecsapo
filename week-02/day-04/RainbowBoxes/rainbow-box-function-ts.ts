b786f8ab7fe5dc33e0e10c802177644f8fe52ed7
Tunde Csapo <tunde.csapo828@gmail.com>
2018-05-17 23:34:46 +0200

    Refactored functions with Actual Rainbow

--- week-02/day-04/RainbowBoxes/rainbow-box-function-ts.ts
+++ week-02/day-04/RainbowBoxes/rainbow-box-function-ts.ts
@@ -3,19 +3,34 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-function drawRainbow(side: number, color: string) {
-  for (let i: number = 600; i >= side; i-=10) {
-    let chars = '0123456789ABCDEF';
-    let randomColor = '#';
-    if (i === side){
-      randomColor = color
-    } else {
-      for (let j: number = 0; j < 6; j++) {
-      randomColor += chars[Math.floor(Math.random() * 16)];
-      }
-    }  
-    ctx.fillStyle = randomColor;
-    ctx.fillRect(300 - i/2, 200-i/2, i, i);
-  }
+//Refactored solution with ACTUAL RAINBOW
+let side: number = 30;
+let frequency = .02;
+
+function drawRainbow(currSize: number, color: string) {
+    ctx.fillStyle = color;
+    ctx.fillRect(300 - currSize/2, 200-currSize/2, currSize, currSize);
 }
-drawRainbow(50, '#ef629f');
+
+for (let i: number = canvas.width; i >= side; i-=10) {
+  let red: number = Math.sin(frequency*i + 0) * 127 + 128;
+  let green: number = Math.sin(frequency*i + 2*Math.PI/3) * 127 + 128;
+  let blue: number = Math.sin(frequency*i + 4*Math.PI/3) * 127 + 128;
+  let randomColor: string = `rgb(${red},${green},${blue})`;
+  drawRainbow(i, randomColor);
+}
+
+/* Original solution with random colors
+for (let i: number = canvas.width; i >= side; i-=10) {
+  let chars = '0123456789ABCDEF';
+  let randomColor = '#';
+  if (i === side){
+    randomColor = color
+  } else {
+    for (let j: number = 0; j < 6; j++) {
+      randomColor += chars[Math.floor(Math.random() * 16)];
+    }
+  }  
+  drawRainbow(i, randomColor);
+}
+*/
