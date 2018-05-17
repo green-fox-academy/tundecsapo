899d7d20a2a4224a0f36f3c1e8ea3875e4b93dc1
Tunde Csapo <tunde.csapo828@gmail.com>
2018-05-17 19:20:39 +0200

    Refactored function, corrected solution

--- week-02/day-04/Horizontal/horizontal-lines-ts.ts
+++ week-02/day-04/Horizontal/horizontal-lines-ts.ts
@@ -3,14 +3,16 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
+//Refactored, corrected solution
 function drawHorizontal(hor, vert) {
-  for (let i: number = 0; i < 3; i++) {
-    ctx.strokeStyle = "tomato";
-    ctx.beginPath();
-    ctx.moveTo(hor, vert);
-    ctx.lineTo(hor + 400, vert);
-    ctx.stroke();
-    vert += 100;
-  }
+  ctx.beginPath();
+  ctx.moveTo(hor, vert);
+  ctx.lineTo(hor + 50, vert);
+  ctx.stroke();
 }
-drawHorizontal(100, 100);
+
+for (let i: number = 0; i < 3; i++) {
+  let x: number = Math.floor(Math.random() * (canvas.width - 50));
+  let y: number = Math.floor(Math.random() * (canvas.height));
+  drawHorizontal(x, y);
+}
