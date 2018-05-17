b4d59e0eced8f794071e12a037a645413bdcef60
Tunde Csapo <tunde.csapo828@gmail.com>
2018-05-17 18:06:52 +0200

    Refactored code, corrected square position

--- week-02/day-04/CenteredBoxes/center-box-function-ts.ts
+++ week-02/day-04/CenteredBoxes/center-box-function-ts.ts
@@ -3,13 +3,21 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-function drawSquare(side){
-  let y: number = (400 - 20 - side * 3)/2;
-  for (let i: number = 0; i < 3; i++) {  
-    ctx.fillStyle = 'tomato';
-    ctx.fillRect(300 - side/2, y, side, side);
-    y += side + 10;
-    console.log(y);
-  }
+function drawSquare(side, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(canvas.width/2 - side/2, canvas.height/2 - side/2, side, side);
 }
-drawSquare(100);
+
+//I used random starting size for the rectangle, that is smaller then the canvas height
+let size: number = Math.floor(Math.random() * (400 - 120 + 1)) + 120;
+for (let i: number = 0; i < 3; i++) {
+  let chars = '0123456789ABCDEF';
+  let randomColor = '#';
+  for (let j: number = 0; j < 6; j++) {
+      randomColor += chars[Math.floor(Math.random() * 16)];
+  }
+  drawSquare(size, randomColor);
+  //reducing random number from the size
+  let reduction: number = Math.floor(Math.random() * (50 - 10 + 1)) + 10;
+  size -= reduction;
+}
