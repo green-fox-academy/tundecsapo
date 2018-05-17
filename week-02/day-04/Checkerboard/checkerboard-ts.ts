b5a74c5e95cdfcab23237df056ea41539697b9b3
Tunde Csapo <tunde.csapo828@gmail.com>
2018-05-17 18:20:15 +0200

    Refactored functions

--- week-02/day-04/Checkerboard/checkerboard-ts.ts
+++ week-02/day-04/Checkerboard/checkerboard-ts.ts
@@ -3,9 +3,25 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
+//Refactored solution:
+let side: number = 20;
+for (let y: number = 0; y < canvas.height; y += side) {
+  for (let x: number = 0; x < canvas.width; x += side) { 
+    let checker: number = (x+y)/side;   
+    if (checker === 0 || checker % 2 == 0) { 
+      drawCheckerboard(x, y, side)
+    } 
+  }
+}
+
+function drawCheckerboard(x, y, side) {
+  ctx.fillRect(x, y, side, side);
+}
+
+/*Original solution:
 function drawCheckerboard(side: number) {
-  for (let y: number = 0; y < 400; y += side){
-    for (let x: number = 0; x < 600; x += side){ 
+  for (let y: number = 0; y < 400; y += side) {
+    for (let x: number = 0; x < 600; x += side) { 
       let checker: number = (x+y)/side;   
       if (checker === 0 || checker % 2 == 0) { 
         ctx.fillRect(x, y, side, side);
@@ -13,4 +29,4 @@ function drawCheckerboard(side: number) {
     }
   }
 }
-drawCheckerboard(20);
+drawCheckerboard(20);*/
