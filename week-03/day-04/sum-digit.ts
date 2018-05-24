7c925a5bcd0f9571a1fa4728b546a081f253c0cf
Tunde Csapo <tunde.csapo828@gmail.com>
2018-05-24 10:53:43 +0200

    Corrected function/removed variable

--- week-03/day-04/sum-digit.ts
+++ week-03/day-04/sum-digit.ts
@@ -1,11 +1,7 @@
-let result: number = 0;
-
 function sumDigits(n: number) {
   if (n >= 0 && n <= 9) {
-    result += n;  
-    return result;
+    return n;
   }
-  result += n % 10;
-  return sumDigits(Math.floor(n / 10));
+  return n % 10 + sumDigits(Math.floor(n / 10));
 }
-console.log(sumDigits(12345));
+console.log(sumDigits(123456));
