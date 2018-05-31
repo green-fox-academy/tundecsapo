ffb37979daf07bbc1701414f803c2c7dce4ff100
Tunde Csapo <tunde.csapo828@gmail.com>
2018-05-31 16:23:03 +0200

    Test file for 'Fibonacci' added

--- week-04/day-04/fibonacci-test.ts
+++ week-04/day-04/fibonacci-test.ts
@@ -20,3 +20,28 @@ test('get fibonacci number', t => {
   t.equal(getFibonacci(testNum), 21);
   t.end();
 })
+
+test('get fibonacci number', t => {
+  t.equal(getFibonacci(true), 'The input is not a number. This method only works for numbers as input.');
+  t.end();
+})
+
+test('get fibonacci number', t => {
+  t.equal(getFibonacci(null), 'The input is not a number. This method only works for numbers as input.');
+  t.end();
+})
+
+test('get fibonacci number NaN NaN NaN NaN', t => {
+  t.equal(getFibonacci(NaN), 'This metod only works for 0 or positive integers.');
+  t.end();
+})
+
+test('get fibonacci number', t => {
+  t.equal(getFibonacci(-2), 'This metod only works for 0 or positive integers.');
+  t.end();
+})
+
+test('get fibonacci number', t => {
+  t.equal(getFibonacci(1/2), 'This metod only works for 0 or positive integers.');
+  t.end();
+})
