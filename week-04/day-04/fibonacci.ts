e772742156113661e0008136158901cccb061398
Tunde Csapo <tunde.csapo828@gmail.com>
2018-05-31 16:22:40 +0200

    Project file for 'Fibonacci' added

--- week-04/day-04/fibonacci.ts
+++ week-04/day-04/fibonacci.ts
@@ -2,11 +2,17 @@
 
 export function getFibonacci(num: any): any {
   if (typeof num === 'undefined') {
-    return 'The input is empty. This method only works for numbers as input.';
+    return 'The input is empty. This method only works for numbers as input.'
   } else if (typeof num === 'string') {
-    return 'The input is a string. This method only works for numbers as input.';
-  } else if (typeof num === 'number' && num >= 0 && num <=1) {
-    return num;
-  } 
-  return getFibonacci(num-2) + getFibonacci(num-1);  
+    return 'The input is a string. This method only works for numbers as input.'
+  } else if (num === null || typeof num === 'boolean') {
+    return 'The input is not a number. This method only works for numbers as input.'
+  } else if (num < 0 || num % 1 !== 0 ) {
+    return 'This metod only works for 0 or positive integers.'
+  } else if (typeof num === 'number' && num >= 0) {
+    if (num >= 0 && num <= 1) {
+      return num;
+    } 
+    return getFibonacci(num - 2) + getFibonacci(num - 1);
+  }
 }
