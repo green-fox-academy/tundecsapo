aa1b9e0e4dcf22a7b940a37ae5f7d2c14d3e25aa
Tunde Csapo <tunde.csapo828@gmail.com>
2018-06-20 14:37:25 +0200

    JS file for 'SVG 1 exercise' added

---
+++ week-06/day-03/svg1-script.js
@@ -0,0 +1,7 @@
+//1) Select the rectangle by the ID: "very_rectangle"
+let rectangle = document.getElementById('very_rectangle');
+//2) Change the rectangle's position to x:50, y:50
+rectangle.setAttribute('x', 50);
+rectangle.setAttribute('y', 50);
+//3) Change its fill to tomato
+rectangle.style.fill = "tomato";
