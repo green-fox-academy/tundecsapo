879c18bea751f552398816cc1de5ca8681f8e171
Tunde Csapo <tunde.csapo828@gmail.com>
2018-06-20 14:58:07 +0200

    files organised in separate folders

--- week-06/day-03/content1-script.js
+++
@@ -1,8 +0,0 @@
-let paragraphs = document.querySelectorAll('p');
-/*for (let i = 0; i < paragraphs.length-1; i++) {
-  paragraphs[i].innerText = paragraphs[paragraphs.length-1].innerText;  
-}*/
-
-for (let i = 0; i < paragraphs.length-1; i++) {
-  paragraphs[i].innerHTML = paragraphs[paragraphs.length-1].innerHTML;  
-}
--- week-06/day-03/greenfox-logo-script.js
+++
@@ -1,5 +0,0 @@
-let leftEye = document.getElementById('left_eye');
-let rightEye = document.getElementById('right_eye')
-
-leftEye.classList.add('madEyes');
-rightEye.classList.add('madEyes');    
