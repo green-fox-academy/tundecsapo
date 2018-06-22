43de857a844b619fb93735dfef65b20914ef5b60
Tunde Csapo <tunde.csapo828@gmail.com>
2018-06-22 15:12:13 +0200

    Initial stage of 'Gallery Project' added

---
+++ week-06/day-05/GalleryProject/server.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.use('/static', express.static('static'));
+
+app.get('/', (req, res) => {
+  const newyorksum = [
+    {
+      title: 'New York City', 
+      story: 'The City of New York, often called New York City (NYC) or simply New York, is the most populous city in the United States.[9] With an estimated 2017 population of 8,622,698[7] distributed over a land area of about 302.6 square miles.', 
+      image: 'newyorkcity'
+    },
+    {
+      title: 'Brooklyn Bridge', 
+      story: 'The Brooklyn Bridge is a hybrid cable-stayed/suspension bridge in New York City and is one of the oldest roadway bridges in the United States.', 
+      image: 'brooklynbridge'
+    },    
+    {
+      title: 'Central Park', 
+      story: 'Central Park is an urban park in Manhattan, New York City. Central Park is the most visited urban park in the United States, and one of the most filmed locations in the world.', 
+      image: 'centralpark'
+    },
+    {
+      title: 'Empire State Building', 
+      story: 'The Empire State Building is a 102-story Art Deco skyscraper in Midtown Manhattan, New York City. Designed by Shreve, Lamb & Harmon and completed in 1931.', 
+      image: 'empirestatebuilding'
+    },
+    {
+      title: 'Statue Of Liberty', 
+      story: 'The Statue of Liberty is a colossal neoclassical sculpture on Liberty Island in New York Harbor in New York City, in the United States.', 
+      image: 'statueofliberty'
+    },
+    {
+      title: 'Times Square', 
+      story: 'Times Square is a major commercial intersection, tourist destination, entertainment center and neighborhood in the Midtown Manhattan section of New York City at the junction of Broadway and Seventh Avenue.', 
+      image: 'timessquare'
+    },    
+    {
+      title: 'Streets', 
+      story: 'Most of Manhattan is laid out in a grid pattern, meaning that it\'s easy to find your way around. Avenues run north-south and streets are east-west. Fifth Avenue separates the East and West sides, with street numbers increasing as you head away from Fifth. Broadway cuts through the city on a diagonal.', 
+      image: 'streets'
+    },    
+    {
+      title: 'Radio City', 
+      story: 'Radio City Music Hall is an entertainment venue located at 1260 Avenue of the Americas at Rockefeller Center in Midtown Manhattan, New York City. Nicknamed the Showplace of the Nation, it was for a time the leading tourist destination in the city.', 
+      image: 'radiocity'
+    },       
+  ];
+  
+  res.render('gallery', {
+    newyork: newyorksum,
+  });
+});
+
+app.listen(PORT, () => {
+console.log(`Listening on port ${PORT}`);
+});
