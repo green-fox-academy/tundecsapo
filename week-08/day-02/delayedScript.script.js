9e8d61311e6e6423064ee8df2baadcc7d992cbb4
Tunde Csapo <tunde.csapo828@gmail.com>
2018-07-03 11:16:45 +0200

    Solution for 'Delayed Click' exercise added

---
+++ week-08/day-02/delayedScript.script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//Create a simple HTML document with one button. If the user clicks the button 
+//it should wait 2 seconds and it should show a text under the button: 2 seconds ellapsed
+const text = document.createElement('p');
+text.textContent = 'You clicked a button!'
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  setTimeout(() => showText(text), 2000)  
+})
+
+function showText(element) {
+  document.body.appendChild(element);
+}
