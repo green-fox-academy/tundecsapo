55a86b2782f5bb41281924f479f0ddec2bb261dc
Tunde Csapo <tunde.csapo828@gmail.com>
2018-07-05 10:41:59 +0200

    Clearing the input fields after signing up

--- week-08/day-03/newsletter/server.js
+++ week-08/day-03/newsletter/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 app.use('/static', express.static('static'));
 app.use(express.json());
 
